91a4d78958e96d872a63f73a47b00a1de6cdcd79
nilay <nilaytayade69@gmail.com>
2024-04-02 10:13:55 +0530

    2

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ const express = require("express");
 
 const app = express();
 const PORT = 4000;
+emails = [];
+express.request(json());
 
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `);
 });
 
 app.get("/", (req, res) => {
-  res.send("Hey this is my API running 🥳");
+  res.send(emails);
 });
 
-app.get("/about", (req, res) => {
-  res.send("This is my about route..... ");
+app.get("/pushemail", (req, res) => {
+  emails.push(req.body);
+  res.send(emails);
 });
 
 // Export the Express API
