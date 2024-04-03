fcd02510084b633444289ffd5431d0bce3ce7973
nilay <nilaytayade69@gmail.com>
2024-04-03 19:29:54 +0530

    dart

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const PORT = 4000;
 emails = [];
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `);
-});
-
 app.get("/", (req, res) => {
   res.send(emails);
 });
@@ -20,5 +16,20 @@ app.post("/pushemail", (req, res) => {
   res.send(emails);
 });
 
+app.get("/darttest", (req, res) => {
+  res.json({
+    username: "ak1932",
+    email: "aryankadole95@gmail.com",
+    first_name: "Aryan Kadole",
+    last_name: "",
+    current_level: 1,
+    hidden_on_leaderboard: false,
+    id: 1190,
+    keys: 2,
+  });
+});
 // Export the Express API
-module.exports = app;
+
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `);
+});
